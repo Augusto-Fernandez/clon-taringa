ba031ceaca0a80880e4f14d8f42ddd68eba1474a
Augusto Fernandez <augusfernandez23@gmail.com>
2024-03-14 18:15:25 -0300

    Se creó body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300 mx-20 rounded-lg space-x-10">
+      <div className="bg-red-800 w-1/2 flex-grow rounded-md p-3 space-y-3">
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
+
+        </div>
+      </div>
+      <div className="w-1/4 flex-grow rounded-md space-y-10">
+        <div className="bg-red-800 h-96 flex-grow rounded-md">
+
+        </div>
+        <div className="bg-red-800 h-96 flex-grow rounded-md">
+
+        </div>
+      </div>
+      <div className="bg-red-800 w-1/4 flex-grow rounded-md">
+
+      </div>
     </main>
   );
 }
