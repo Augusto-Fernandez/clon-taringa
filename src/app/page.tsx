9cd3bb5cef3f3ee25a13bb7f01265416fdecb5de
Augusto Fernandez <augusfernandez23@gmail.com>
2024-03-15 16:52:51 -0300

    Se creó componente postcard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,26 @@
+import PostCard from "@/components/PostCard";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300 mx-20 rounded-lg space-x-10">
-      <div className="bg-red-800 w-1/2 flex-grow rounded-md p-3 space-y-3">
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
+      <div className="w-1/2 flex-grow">
+        <div className="h-10">
+          <p>Crear Post</p>
         </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
+        <div className="bg-red-800 p-3 space-y-3 rounded-md">
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
         </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
-        </div>
-        <div className="bg-slate-300 h-16 flex-grow rounded-lg">
-
+        <div className="h-10">
+          <p>Boton para cambiar pagina</p>
         </div>
       </div>
       <div className="w-1/4 flex-grow rounded-md space-y-10">
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,15 @@
+//import Link from "next/link";
+
+/*
+<Link href={"/post/"+post.id}>
+        
+</Link>
+*/
+
+export default function PostCard() {
+    return(
+        <div className="bg-slate-300 h-14 rounded-lg">
+            <p>Post de prueba</p>
+        </div>
+    );
+}
