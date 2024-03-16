c0ea6cc95b5a1df8b88262f99da90264674c8981
Augusto Fernandez <augusfernandez23@gmail.com>
2024-03-16 16:52:08 -0300

    Se creó pagina de creación de post

---
+++ src/app/createpost/page.tsx
@@ -0,0 +1,25 @@
+export default function CreatePost (){
+    return(
+        <div className="min-h-screen bg-gray-100">
+            <div className="mx-60 p-5 space-y-5">
+                <h1 className="text-slate-600 font-semibold text-4xl">Crear Post</h1>
+                <div className="flex space-x-3">
+                    <div className="w-2/3 bg-white rounded-t-md border border-gray-400">
+                        <input type="text" placeholder="Título (obligatorio)" className="input h-14 hover:no-animation focus:outline-none w-full" />
+                        <div className="bg-gray-100 pl-4">
+                            <div className="flex h-14 p-4 space-x-10">
+                                <p className=" text-sm border-l border-gray-200 pl-8">Imagen</p>
+                                <p className=" text-sm border-l border-gray-200 pl-8">Video</p>
+                                <p className=" text-sm border-l border-gray-200 pl-8">Link</p>
+                            </div>
+                        </div>
+                        <textarea className="textarea w-full min-h-96 max-h-96 hover:no-animation focus:outline-none" placeholder="Contenido del post (obligatorio)"></textarea>
+                    </div>
+                    <div className="w-1/3 bg-white rounded-md border border-gray-400">
+                        <p>Prueba</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import PostCard from "@/components/PostCard";
 
 export default function Home() {
@@ -5,9 +7,11 @@ export default function Home() {
     <main className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300 mx-20 rounded-lg space-x-10">
       <div className="w-1/2 flex-grow">
         <div className="h-14">
-          <button className="btn text-base font-semibold glass w-1/4 bg-green-500 border-1 border-green-300/80 text-white hover:bg-green-600">
-            + Crear Post
-          </button>
+          <Link href={"/createpost"}>
+            <button className="btn text-base font-semibold glass w-1/4 bg-green-500 border border-green-300/80 text-white hover:bg-green-600">
+              + Crear Post
+            </button>
+          </Link>
         </div>
         <div className="bg-red-800 p-3 space-y-3 rounded-md">
           <PostCard/>
