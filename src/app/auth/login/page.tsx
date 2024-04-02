638ea69e0fe32e5b91d0fc71f18f388501027d8c
Augusto Fernandez <augusfernandez23@gmail.com>
2024-04-02 16:14:36 -0300

    Se creo pagina de login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,31 +1,22 @@
 "use client"
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
 
 type Inputs = {
     username: string
-    email: string;
     password: string;
-    confirmPassword: string;
 };
 
-export default function RegisterPage() {
+export default function LoginPage() {
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-    const router = useRouter();
-
     const onSubmit:SubmitHandler<Inputs> = async (data) => {
-        if (data.password !== data.confirmPassword) {
-            return alert("Contraseñas no coinciden");
-        }
-
-        router.push("/auth/login");
+        console.log(JSON.stringify(data));
     };
 
     return (
         <div className="min-h-screen max-h-screen flex justify-center items-center bg-gray-100">
             <form onSubmit={handleSubmit(onSubmit)} className="w-1/4 bg-white p-10 rounded-md border border-gray-400">
-                <h1 className="text-slate-600 font-bold text-4xl mb-4">Crear Usuario</h1>
+                <h1 className="text-slate-600 font-bold text-4xl mb-4">Iniciar Sesión</h1>
                 <div className="mb-4 space-y-2">
                     <label className="text-slate-600 block text-sm">Nombre de usuario:</label>
                     <input
@@ -42,25 +33,6 @@ export default function RegisterPage() {
                     {errors.username && typeof errors.username.message === 'string' && (
                         <span className="text-red-500 text-xs font-bold">
                             {errors.username.message}
-                        </span>
-                    )}
-                </div>
-                <div className="mb-4 space-y-2">
-                    <label className="text-slate-600 block text-sm">Email:</label>
-                    <input
-                        type="email"
-                        className="input p-3 rounded block w-full border border-gray-300 hover:no-animation focus:outline-non"
-                        placeholder="Email"
-                        {...register("email", {
-                            required: {
-                                value: true,
-                                message: "Email es requerido",
-                            },
-                        })}
-                    />
-                    {errors.email && typeof errors.email.message === 'string' && (
-                        <span className="text-red-500 text-xs font-bold">
-                            {errors.email.message}
                         </span>
                     )}
                 </div>
@@ -83,27 +55,8 @@ export default function RegisterPage() {
                         </span>
                     )}
                 </div>
-                <div className="mb-4 space-y-2">
-                    <label className="text-slate-600 mb-2 block text-sm">Confirmar Contraseña:</label>
-                    <input
-                        type="password"
-                        className="input p-3 rounded block mb-2 w-full border border-gray-300 hover:no-animation focus:outline-non"
-                        placeholder="********"
-                        {...register("confirmPassword", {
-                            required: {
-                            value: true,
-                            message: "Es obligatorio confirmar contraseña",
-                            },
-                        })}
-                    />
-                    {errors.confirmPassword && typeof errors.confirmPassword.message === 'string' && (
-                        <span className="text-red-500 text-xs font-bold">
-                            {errors.confirmPassword.message}
-                        </span>
-                    )}
-                </div>
                 <button type="submit" className="btn w-full text-base font-semibold bg-green-500 border border-green-300/80 text-white hover:bg-green-600 mt-2">
-                    Registrar
+                    Ingresar
                 </button>
             </form>
         </div>
