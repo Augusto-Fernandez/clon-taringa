47c89d1a4d5039277597a5265f46b2155b2ac811
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-20 18:44:35 -0300

    Se modifico el css de home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,80 +95,82 @@ export default async function Home({searchParams:{page = "1"}}: HomeProps) {
   const top10Posts = postsSortedByVotes.slice(0, 5);
   
   return (
-    <main className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300 mx-20 rounded-lg space-x-10">
-      <div className="w-1/2 flex-grow">
-        <div className="h-14 flex justify-between">
-          <form action={sortPosts}>
-            <div className="flex space-x-16">
-              <p className="p-3 text-xl font-bold">Posts</p>
-              <div className="flex items-center space-x-1">
-                <SelectCategoty
-                  className="dropdown z-[1] menu p-2 shadow bg-slate-200 w-52 rounded"
-                />
-                <SearchButton
-                  className="btn-ghost h-[2rem] w-[2rem] bg-green-500 rounded-lg hover:bg-green-400"
-                  svgSize="w-6 h-6"
-                />
-              </div>
-            </div>
-          </form>
-          <Link href={"/createpost"}>
-              <button className="btn text-base font-semibold glass bg-green-500 border border-green-300/80 text-white hover:bg-green-600">
-                  + Crear Post
-              </button>
-          </Link>
-        </div>
-        <div className="bg-red-800 h-[41.25rem] p-3 rounded-md">
-          {
-            posts.map(post => (
-              <PostCard post={post} key={post.id}/>
-            ))
-          }
-        </div>
-        <div className="h-10 flex justify-center mt-2">
-          {
-              totalPages>1 ? (
-                  <PaginationBar 
-                      currentPage={currentPage} 
-                      totalPages={totalPages}
+    <main className="bg-gradient-to-r from-purple-200 from-5% via-pink-200 via-30% to-emerald-100 to-95% ...">
+      <div className="flex min-h-screen items-stretch justify-between bg-slate-300/[.45] p-5 mx-20 rounded-lg space-x-10">
+        <div className="w-1/2 flex-grow">
+          <div className="h-14 flex justify-between">
+            <form action={sortPosts}>
+              <div className="flex space-x-16">
+                <p className="p-3 text-xl font-bold">Posts</p>
+                <div className="flex items-center space-x-1">
+                  <SelectCategoty
+                    className="dropdown z-[1] menu p-2 shadow bg-slate-200/[.20] w-52 rounded"
                   />
-              ) : (
-                <div className="join-item btn">1</div>
-              )
-          }
-        </div>
-      </div>
-      <div className="w-1/4 rounded-md space-y-1">
-        <div className="space-y-1">
-          <p className="p-3 text-xl font-bold">Noticias</p>
-          <div className="bg-red-800 h-52 rounded-md p-3">
+                  <SearchButton
+                    className="btn-ghost h-[2rem] w-[2rem] bg-green-500/50 rounded-lg hover:bg-green-400"
+                    svgSize="w-6 h-6"
+                  />
+                </div>
+              </div>
+            </form>
+            <Link href={"/createpost"}>
+                <button className="btn text-sm font-semibold glass bg-green-500/65 border border-green-300/80 text-white hover:bg-green-600">
+                    + Crear Post
+                </button>
+            </Link>
+          </div>
+          <div className="bg-slate-400/[.10] h-[41.25rem] p-3 rounded-md">
             {
-              adminPosts.map(post => (
-                <AdminPostCard
-                  key={post.id}
-                  post={post}
-                />
+              posts.map(post => (
+                <PostCard post={post} key={post.id}/>
               ))
             }
           </div>
-        </div>
-        <div>
-          <p className="p-3 text-xl font-bold">Top Posts</p>
-          <div className="bg-red-800 h-[24.80rem] rounded-md p-3 space-y-1.5">
+          <div className="h-10 flex justify-center mt-2">
             {
-              top10Posts.map(post => (
-                <TopPostCard 
-                  key={post.id}
-                  post={post}
-                  voteRatio={post.voteRatio}
-                />
-              ))
+                totalPages>1 ? (
+                    <PaginationBar 
+                        currentPage={currentPage} 
+                        totalPages={totalPages}
+                    />
+                ) : (
+                  <div className="join-item btn">1</div>
+                )
             }
           </div>
         </div>
-      </div>
-      <div className="bg-red-800 w-1/4 flex-grow rounded-md">
+        <div className="w-1/4 rounded-md space-y-1">
+          <div className="space-y-1">
+            <p className="p-3 text-xl font-bold">Noticias</p>
+            <div className="bg-slate-400/[.10] h-52 rounded-md p-3">
+              {
+                adminPosts.map(post => (
+                  <AdminPostCard
+                    key={post.id}
+                    post={post}
+                  />
+                ))
+              }
+            </div>
+          </div>
+          <div>
+            <p className="p-3 text-xl font-bold">Top Posts</p>
+            <div className="bg-slate-400/[.10] h-[24.80rem] rounded-md p-3 space-y-1.5">
+              {
+                top10Posts.map(post => (
+                  <TopPostCard 
+                    key={post.id}
+                    post={post}
+                    voteRatio={post.voteRatio}
+                  />
+                ))
+              }
+            </div>
+          </div>
+        </div>
+        <div className="bg-red-800 w-1/4 flex-grow rounded-md">
 
+        </div>
       </div>
     </main>
   );
--- src/components/AdminPostCard.tsx
+++ src/components/AdminPostCard.tsx
@@ -18,7 +18,7 @@ export default async function AdminPostCard({post}:AdminPostProps) {
     });
     
     return(
-        <Link href={"/post/"+post.id} className="bg-slate-300 h-14 rounded-lg flex mb-2">
+        <Link href={"/post/"+post.id} className="bg-slate-200/[.55] h-14 rounded-lg flex mb-2">
             <Image 
                 src={postDefaultBanner} 
                 width={80}
