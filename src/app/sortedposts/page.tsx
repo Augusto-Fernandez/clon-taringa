1002e911ba44c238395cb3c222594ad9e202118d
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-22 18:19:27 -0300

    Se modifico css en seachpost

--- src/app/sortedposts/page.tsx
+++ src/app/sortedposts/page.tsx
@@ -106,82 +106,84 @@ export default async function SortedPosts({searchParams: { query, page = "1" }}:
   const top10Posts = postsSortedByVotes.slice(0, 10);
   
   return (
-    <main className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300 mx-20 rounded-lg space-x-10">
-      <div className="w-1/2 flex-grow">
-        <div className="h-14 flex justify-between">
-          <form action={sortPosts}>
-            <div className="flex space-x-16">
-              <p className="p-3 text-xl font-bold">Posts</p>
-              <div className="flex items-center space-x-1">
-                <SelectCategoty
-                  className="dropdown z-[1] menu p-2 shadow bg-slate-200 w-52 rounded"
-                />
-                <SearchButton
-                  className="btn-ghost h-[2rem] w-[2rem] bg-green-500 rounded-lg hover:bg-green-400"
-                  svgSize="w-6 h-6"
-                />
+    <main className="bg-gradient-to-r from-purple-200 from-5% via-pink-200 via-30% to-emerald-100 to-95% ...">
+      <div className="flex min-h-screen items-stretch justify-between p-5 bg-slate-300/50 mx-20 rounded-lg space-x-10">
+        <div className="w-1/2 flex-grow">
+          <div className="h-14 flex justify-between">
+            <form action={sortPosts}>
+              <div className="flex space-x-16">
+                <p className="p-3 text-xl text-slate-700/90 font-bold">Posts</p>
+                <div className="flex items-center space-x-1">
+                  <SelectCategoty
+                    className="dropdown z-[1] menu p-2 shadow bg-slate-200/[.20] w-52 rounded text-slate-700/90"
+                  />
+                  <SearchButton
+                    className="btn-ghost h-[2rem] w-[2rem] bg-green-500/50 rounded-lg hover:bg-green-400"
+                    svgSize="w-6 h-6"
+                  />
+                </div>
               </div>
+            </form>
+            <Link href={"/createpost"}>
+                <button className="btn text-sm font-semibold glass bg-green-500/65 border border-green-300/80 text-white hover:bg-green-600">
+                    + Crear Post
+                </button>
+            </Link>
+          </div>
+          <div className="bg-slate-400/[.10] h-[41.25rem] p-3 rounded-md">
+            {posts.length > 0 ? (
+              posts.map(post => (
+                <PostCard post={post} key={post.id}/>
+              ))
+              ) : (
+                <p className="p-10 text-3xl font-semibold text-slate-700/90">Aún no hay posts en esta categoria</p>
+              )}
+          </div>
+          <div className="h-10 flex justify-center mt-2">
+            {
+              totalPages>1 ? (
+                <PaginationBar 
+                  currentPage={currentPage} 
+                  totalPages={totalPages}
+                />
+              ) : (
+                <div className="join-item btn">1</div>
+              )
+            }
+          </div>
+        </div>
+        <div className="w-1/4 rounded-md space-y-1">
+          <div className="space-y-1">
+            <p className="p-3 text-xl font-bold text-slate-700/90">Noticias</p>
+            <div className="bg-slate-400/[.10] h-52 rounded-md p-3">
+              {
+                adminPosts.map(post => (
+                  <AdminPostCard
+                    key={post.id}
+                    post={post}
+                  />
+                ))
+              }
             </div>
-          </form>
-          <Link href={"/createpost"}>
-              <button className="btn text-base font-semibold glass bg-green-500 border border-green-300/80 text-white hover:bg-green-600">
-                  + Crear Post
-              </button>
-          </Link>
-        </div>
-        <div className="bg-red-800 h-[41.25rem] p-3 rounded-md">
-          {posts.length > 0 ? (
-            posts.map(post => (
-              <PostCard post={post} key={post.id}/>
-            ))
-            ) : (
-              <p className="p-10 text-3xl font-semibold">Aún no hay posts en esta categoria</p>
-            )}
-        </div>
-        <div className="h-10 flex justify-center mt-2">
-          {
-            totalPages>1 ? (
-              <PaginationBar 
-                currentPage={currentPage} 
-                totalPages={totalPages}
-              />
-            ) : (
-              <div className="join-item btn">1</div>
-            )
-          }
-        </div>
-      </div>
-      <div className="w-1/4 rounded-md space-y-1">
-        <div className="space-y-1">
-          <p className="p-3 text-xl font-bold">Noticias</p>
-          <div className="bg-red-800 h-52 rounded-md p-3">
-            {
-              adminPosts.map(post => (
-                <AdminPostCard
-                  key={post.id}
-                  post={post}
-                />
-              ))
-            }
+          </div>
+          <div>
+            <p className="p-3 text-xl font-bold text-slate-700/90">Top Posts</p>
+            <div className="bg-slate-400/[.10] h-[24.80rem] rounded-md p-3 space-y-1.5">
+              {
+                top10Posts.map(post => (
+                  <TopPostCard 
+                    key={post.id}
+                    post={post}
+                    voteRatio={post.voteRatio}
+                  />
+                ))
+              }
+            </div>
           </div>
         </div>
-        <div>
-          <p className="p-3 text-xl font-bold">Top Posts</p>
-          <div className="bg-red-800 h-[24.80rem] rounded-md p-3 space-y-1.5">
-            {
-              top10Posts.map(post => (
-                <TopPostCard 
-                  key={post.id}
-                  post={post}
-                  voteRatio={post.voteRatio}
-                />
-              ))
-            }
-          </div>
-        </div>
-      </div>
-      <div className="bg-red-800 w-1/4 flex-grow rounded-md">
+        <div className="bg-red-800 w-1/4 flex-grow rounded-md">
 
+        </div>
       </div>
     </main>
   );
