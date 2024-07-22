d579958968a7c3b9e6ca70ac288c1f4a5b5b25d0
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-22 16:38:41 -0300

    Se modifico css en searchchat

--- src/app/searchchat/page.tsx
+++ src/app/searchchat/page.tsx
@@ -146,47 +146,50 @@ export default async function MessagesPage ({searchParams:{query, page = "1"}}:M
     }
     
     return(
-        <div className="min-h-screen bg-gray-100 flex justify-center">
-            <div className=" min-h-screen w-2/3 bg-slate-300 mx-20 rounded-lg justify-center">
-                <div className="pt-10 pl-10 flex">
-                    <h1 className="text-slate-600 font-semibold text-4xl">Mensajes</h1>
-                </div>
-                <div className="bg-red-800 h-[41.25rem] rounded-md mt-10 mx-10 mb-2 p-3">
-                    {
-                        conversationsArray.length > 0 ? (
-                            conversationsArray.map(chat => (
-                                <ChatCard 
-                                    key={ chat.id}
-                                    otherUserName={getOtherUserName(chat.userIds)}
-                                    chatId={chat.id}
-                                    messageUserName={getMessageUserName(chat.id)}
-                                    body={getLastMessageBody(chat.id)}
-                                    date={getLastMessageDate(chat.id)}
-                                    notifCount={getMessageNotification(chat.id)}
-                                    userId={userLogged?.id as string}
-                                    handleChatRedirect={handleChatRedirect}
+        <main className="bg-gradient-to-r from-purple-100 from-5% via-pink-200 via-30% to-emerald-100 to-95% ...">
+            <div className="min-h-screen flex justify-center">
+                <div className=" min-h-screen w-2/3 bg-slate-300/50 mx-20 rounded-lg justify-center">
+                    <div className="pt-10 pl-10 flex">
+                        <h1 className="text-slate-700/90 font-semibold text-3xl">Mensajes</h1>
+                    </div>
+                    <div className="bg-slate-400/10 h-[41.25rem] rounded-md mt-10 mx-10 mb-2 p-3">
+                        {
+                            conversationsArray.length > 0 ? (
+                                conversationsArray.map(chat => (
+                                    <ChatCard 
+                                        key={ chat.id}
+                                        otherUserName={getOtherUserName(chat.userIds)}
+                                        chatId={chat.id}
+                                        messageUserName={getMessageUserName(chat.id)}
+                                        body={getLastMessageBody(chat.id)}
+                                        date={getLastMessageDate(chat.id)}
+                                        notifCount={getMessageNotification(chat.id)}
+                                        userId={userLogged?.id as string}
+                                        handleChatRedirect={handleChatRedirect}
+                                    />
+                                ))
+                            ) : (
+                                <div className="w-full flex justify-center">
+                                    <p className="p-10 text-5xl font-semibold">No se encontró chat</p>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="h-14 flex justify-center">
+                        {
+                            totalPages>1 ? (
+                                <PaginationBar 
+                                    currentPage={currentPage} 
+                                    totalPages={totalPages}
+                                    query={query}
                                 />
-                            ))
-                        ) : (
-                            <div className="w-full flex justify-center">
-                                <p className="p-10 text-5xl font-semibold">No se encontró chat</p>
-                            </div>
-                        )
-                    }
-                </div>
-                <div className="h-14 flex justify-center">
-                    {
-                        totalPages>1 ? (
-                            <PaginationBar 
-                                currentPage={currentPage} 
-                                totalPages={totalPages}
-                            />
-                        ) : (
-                            <div className="join-item btn">1</div>
-                        )
-                    }
+                            ) : (
+                                <div className="join-item btn">1</div>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 };
