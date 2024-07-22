bde019ec4e584ecad03c95d027aec313a13c27b0
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-22 16:27:42 -0300

    Se modifico css en search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -41,32 +41,35 @@ export default async function SearchPage({searchParams: { query, page = "1" }}: 
     const totalPages = Math.ceil(totalPostCount/pageSize);
     
     return(
-        <div className="min-h-screen bg-gray-100 flex justify-center">
-            <div className=" min-h-screen w-2/3 bg-slate-300 mx-20 rounded-lg justify-center">
-                <div className="bg-red-800 h-[41.25rem] rounded-md mt-10 mx-10 mb-2 p-3">
-                    {posts.length > 0 ? (
-                            posts.map(post => (
-                                <PostCard post={post} key={post.id}/>
-                            ))
-                        ) : (
-                            <div className="w-full flex justify-center">
-                                <p className="p-10 text-6xl font-semibold">No se encontró post</p>
-                            </div>
-                        )}
-                </div>
-                <div className="h-14 flex justify-center">
-                    {
-                        totalPages>1 ? (
-                            <PaginationBar 
-                                currentPage={currentPage} 
-                                totalPages={totalPages}
-                            />
-                        ) : (
-                            <div className="join-item btn">1</div>
-                        )
-                    }
+        <main className="bg-gradient-to-r from-purple-100 from-5% via-pink-200 via-30% to-emerald-100 to-95% ...">
+            <div className="min-h-screen flex justify-center">
+                <div className=" min-h-screen w-2/3 bg-slate-300/50 mx-20 rounded-lg justify-center">
+                    <div className="bg-slate-400/10 h-[41.25rem] rounded-md mt-10 mx-10 mb-2 p-3">
+                        {posts.length > 0 ? (
+                                posts.map(post => (
+                                    <PostCard post={post} key={post.id}/>
+                                ))
+                            ) : (
+                                <div className="w-full flex justify-center">
+                                    <p className="p-10 text-5xl font-semibold text-slate-700/90">No se encontró post</p>
+                                </div>
+                            )}
+                    </div>
+                    <div className="h-14 flex justify-center">
+                        {
+                            totalPages>1 ? (
+                                <PaginationBar 
+                                    currentPage={currentPage} 
+                                    totalPages={totalPages}
+                                    query={query}
+                                />
+                            ) : (
+                                <div className="join-item btn">1</div>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 }
