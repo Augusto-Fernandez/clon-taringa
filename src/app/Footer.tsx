8aa3b6a335c9579194290222ac73172554664560
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-24 18:28:55 -0300

    Se modifico footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,24 +2,7 @@ export default function Footer() {
     return (
         <footer className="flex p-5 bg-green-300 space-x-5 footer justify-center">
             <div>
-                <span className="footer-title">Services</span>
-                <a className="link-hover link">Branding</a>
-                <a className="link-hover link">Design</a>
-                <a className="link-hover link">Marketing</a>
-                <a className="link-hover link">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link-hover link">About us</a>
-                <a className="link-hover link">Contact</a>
-                <a className="link-hover link">Jobs</a>
-                <a className="link-hover link">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link-hover link">Terms of use</a>
-                <a className="link-hover link">Privacy policy</a>
-                <a className="link-hover link">Cookie policy</a>
+                <span className="text-sm text-green-900">Copyright ©2024 - Designed by Augusto Fernadez</span>
             </div>
         </footer>
     );
