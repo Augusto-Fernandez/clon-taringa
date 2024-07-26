83c399e96401d36c8cbab670de2450a8ed9dd1cd
Augusto Fernandez <augusfernandez23@gmail.com>
2024-07-26 16:44:23 -0300

    Se hizo responsive el footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,8 +1,15 @@
 export default function Footer() {
     return (
-        <footer className="flex p-5 bg-green-300 space-x-5 footer justify-center">
+        <footer className="flex p-5 bg-green-300 footer justify-center">
             <div>
-                <span className="text-sm text-green-900">Copyright ©2024 - Designed by Augusto Fernadez</span>
+                <span 
+                    className="
+                        text-xs text-green-900
+                        sm:text-sm
+                    "
+                >
+                    Copyright ©2024 - Designed by Augusto Fernadez
+                </span>
             </div>
         </footer>
     );
