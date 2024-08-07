7427468495615e24689839157535ae6a5d1eef45
Augusto Fernandez <augusfernandez23@gmail.com>
2024-08-07 18:36:42 -0300

    Se hizo responsive navbar

--- src/app/navbar/UserMenu.tsx
+++ src/app/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import NotificationIcon from "@/components/svgs/NotificationIcon";
 import MessageIcon from "@/components/svgs/MessageIcon";
 import FavoritedIcon from "@/components/svgs/FavoritedIcon";
 import AdminPanelIcon from "@/components/svgs/AdminPanelIcon";
+import BurgermenuIcon from "@/components/svgs/BurgermenuIcon";
 
 interface UserMenuButtonProps {
     session: Session | null;
@@ -38,50 +39,116 @@ export default function UserMenu({session, notificationCount, messageNotificatio
             }
             {
                 user ? (
-                    <ul className="flex justify-center items-center h-12 bg-green-400/50 rounded-md">
-                        <li className="border-r border-r-slate-300 p-2 hover:bg-green-500/50 rounded-l-md h-12">
-                            <Link href={"/notifications"}>
-                                <div className="indicator">
-                                    <NotificationIcon
-                                        className="w-8 h-8"
-                                        line="rgb(255, 255, 255)"
-                                    />
-                                    {
-                                        notificationCount > 0 && notificationCount < 10 && (
-                                            <span className="indicator-item badge bg-red-500 text-white">{notificationCount}</span>
-                                        )
-                                    }
-                                    {
-                                        notificationCount > 9 && (
-                                            <span className="indicator-item badge bg-red-500 text-white">+9</span>
-                                        )
-                                    }
-                                </div>
-                            </Link>
-                        </li>
-                        <li className="border-r border-r-slate-300 px-2 pt-2 hover:bg-green-500/50">
-                            <Link href={"/messages"}>
-                                <div className="indicator">
-                                    <MessageIcon/>
-                                    {
-                                        messageNotificationCount > 0 && messageNotificationCount < 10 && (
-                                            <span className="indicator-item badge bg-red-500 text-white">{messageNotificationCount}</span>
-                                        )
-                                    }
-                                    {
-                                        messageNotificationCount > 9 && (
-                                            <span className="indicator-item badge bg-red-500 text-white">+9</span>
-                                        )
-                                    }
-                                </div>
-                            </Link>
-                        </li>
-                        <li  className="p-2 hover:bg-green-500/50 rounded-r-md">
-                            <Link href={"/saved"}>
-                                <FavoritedIcon className="w-8 h-8" background="none" line="white"/>
-                            </Link>
-                        </li>
-                        <li className="dropdown dropdown-end ">
+                    <div className="flex justify-center items-center h-12 bg-green-400/50 rounded-md">
+                        <ul 
+                            className="
+                                hidden
+                                md:flex
+                            "
+                        >
+                            <li className="border-r border-r-slate-300 p-2 hover:bg-green-500/50 rounded-l-md h-12">
+                                <Link href={"/notifications"}>
+                                    <div className="indicator">
+                                        <NotificationIcon
+                                            className="w-8 h-8"
+                                            line="rgb(255, 255, 255)"
+                                        />
+                                        {
+                                            notificationCount > 0 && notificationCount < 10 && (
+                                                <span className="indicator-item badge bg-red-500 text-white">{notificationCount}</span>
+                                            )
+                                        }
+                                        {
+                                            notificationCount > 9 && (
+                                                <span className="indicator-item badge bg-red-500 text-white">+9</span>
+                                            )
+                                        }
+                                    </div>
+                                </Link>
+                            </li>
+                            <li className="border-r border-r-slate-300 px-2 pt-2 hover:bg-green-500/50">
+                                <Link href={"/messages"}>
+                                    <div className="indicator">
+                                        <MessageIcon/>
+                                        {
+                                            messageNotificationCount > 0 && messageNotificationCount < 10 && (
+                                                <span className="indicator-item badge bg-red-500 text-white">{messageNotificationCount}</span>
+                                            )
+                                        }
+                                        {
+                                            messageNotificationCount > 9 && (
+                                                <span className="indicator-item badge bg-red-500 text-white">+9</span>
+                                            )
+                                        }
+                                    </div>
+                                </Link>
+                            </li>
+                            <li  className="p-2 hover:bg-green-500/50 rounded-r-md">
+                                <Link href={"/saved"}>
+                                    <FavoritedIcon className="w-8 h-8" background="none" line="white"/>
+                                </Link>
+                            </li>
+                        </ul>
+                        <div 
+                            className="
+                                dropdown dropdown-bottom
+                                md:hidden
+                            "
+                        >
+                            <button className="p-2">
+                                <BurgermenuIcon/>
+                            </button>
+                            <ul tabIndex={0} className="dropdown-content z-[2] menu bg-green-300 rounded-box">
+                                <li className="p-2 rounded-l-md h-12">
+                                    <Link href={"/notifications"}>
+                                        <div className="indicator">
+                                            <NotificationIcon
+                                                className="w-8 h-8"
+                                                line="rgb(255, 255, 255)"
+                                            />
+                                            {
+                                                notificationCount > 0 && notificationCount < 10 && (
+                                                    <span className="indicator-item badge bg-red-500 text-white">{notificationCount}</span>
+                                                )
+                                            }
+                                            {
+                                                notificationCount > 9 && (
+                                                    <span className="indicator-item badge bg-red-500 text-white">+9</span>
+                                                )
+                                            }
+                                        </div>
+                                    </Link>
+                                </li>
+                                <li className="px-2 pt-2">
+                                    <Link href={"/messages"}>
+                                        <div className="indicator">
+                                            <MessageIcon/>
+                                            {
+                                                messageNotificationCount > 0 && messageNotificationCount < 10 && (
+                                                    <span className="indicator-item badge bg-red-500 text-white">{messageNotificationCount}</span>
+                                                )
+                                            }
+                                            {
+                                                messageNotificationCount > 9 && (
+                                                    <span className="indicator-item badge bg-red-500 text-white">+9</span>
+                                                )
+                                            }
+                                        </div>
+                                    </Link>
+                                </li>
+                                <li  className="p-2 rounded-r-md">
+                                    <Link href={"/saved"}>
+                                        <FavoritedIcon className="w-8 h-8" background="none" line="white"/>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div 
+                            className="
+                                dropdown dropdown-bottom
+                                md:dropdown-end
+                            "
+                        >
                             <Image
                                 tabIndex={0} 
                                 role="button"
@@ -91,16 +158,16 @@ export default function UserMenu({session, notificationCount, messageNotificatio
                                 height={20}
                                 className="w-10 h-10 rounded-full"
                             />
-                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-36 space-y-1">
+                            <ul tabIndex={0} className="dropdown-content z-[2] menu p-2 shadow bg-slate-300/70 rounded-box w-36 space-y-1">
                                 <li>
-                                    <Link href={"/profile?query="+user.name} className="btn">Perfil</Link>
+                                    <Link href={"/profile?query="+user.name} className="btn text-slate-500 border border-slate-400">Perfil</Link>
                                 </li>
                                 <li>
-                                    <button className="btn" onClick={() => signOut({ callbackUrl: "/" })}>Cerrar Sesión</button>
+                                    <button className="btn text-slate-500 border border-slate-400" onClick={() => signOut({ callbackUrl: "/" })}>Cerrar Sesión</button>
                                 </li>
                             </ul>
-                        </li>
-                    </ul>
+                        </div>
+                    </div>
                 ) 
                     : 
                 (
---
+++ src/components/svgs/BurgermenuIcon.tsx
@@ -0,0 +1,10 @@
+export default function BurgermenuIcon() {
+    return(
+        <svg width="2rem" height="2rem" viewBox="0 0 24 24" fill="none">
+            <rect width="24" height="24" fill="rgb(74,222,128/0.5)"/>
+            <path d="M6 12H18" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M6 15.5H18" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M6 8.5H18" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    );   
+};
